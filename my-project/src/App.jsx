48f72c6b6f239a758a253e293d4a919b9f675308
AyushiBadika <badikaayushi@gmail.com>
2024-06-05 19:50:30 +0530

    local to git

---
+++ my-project/src/App.jsx
@@ -0,0 +1,60 @@
+import { useRef, useState } from "react";
+
+export default function App() {
+  const Paragraphs = [
+    "Jelly sweet roll jelly beans biscuit pie macaroon chocolate donut. Carrot cake caramels pie sweet apple pie tiramisu carrot cake. Marzipan marshmallow croissant tootsie roll lollipop. Cupcake lemon drops bear claw gummies. Jelly bear claw gummi bears lollipop cotton candy gummi bears chocolate bar cake cookie. Cupcake muffin danish muffin cookie gummies. Jelly beans tiramisu pudding. Toffee soufflé chocolate cake pastry brownie. Oat cake halvah sweet roll cotton candy croissant lollipop. Macaroon tiramisu chocolate bar candy candy carrot cake jelly sweet. Gummies croissant macaroon dessert. Chocolate cake dragée pie.",
+    "Next level tbh everyday carry, blog copper mug forage kitsch roof party pickled hammock kale chips tofu. Etsy shoreditch 8-bit microdosing, XOXO viral butcher banh mi humblebrag listicle woke bicycle rights brunch before they sold out ramps. Twee shabby chic taiyaki flannel, enamel pin venmo vape four loko. Hexagon kale chips typewriter kitsch 8-bit organic plaid small batch keffiyeh ethical banh mi narwhal echo park cronut.",
+    "Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.",
+    "Cat gets stuck in tree firefighters try to get cat down firefighters get stuck in tree cat eats firefighters' slippers kitty power ignore the squirrels, you'll never catch them anyway for what a cat-ass-trophy! or purr as loud as possible, be the most annoying cat that you can, and, knock everything off the table. Pretend you want to go out but then don't bite off human's toes, yet disappear for four days and return home with an expensive injury; bite the vet so catch eat throw up catch eat throw up bad birds.",
+    "This opera's as lousy as it is brilliant! Your lyrics lack subtlety. You can't just have your characters announce how they feel. That makes me feel angry! Anyhoo, your net-suits will allow you to experience Fry's worm infested bowels as if you were actually wriggling through them. I just told you! You've killed me! Fry! Quit doing the right thing, you jerk! Michelle, I don't regret this, but I both rue and lament it. Morbo can't understand his teleprompter because he forgot how you say that letter that's shaped like a man wearing a hat.",
+    "Airedale hard cheese mozzarella. Pecorino melted cheese port-salut emmental babybel cheese and wine melted cheese manchego. Everyone loves blue castello everyone loves fromage cheese slices airedale cheddar cream cheese. Bavarian bergkase who moved my cheese halloumi port-salut gouda jarlsberg ricotta rubber cheese. Stinking bishop smelly cheese brie.",
+    "Salvia glossier subway tile, leggings mustache YOLO semiotics chia. Pitchfork tbh af blog church-key meggings vaporware PBR&B master cleanse post-ironic man bun pabst mustache letterpress synth. Snackwave raw denim godard, 3 wolf moon shaman offal kitsch unicorn live-edge selvage schlitz fashion axe vaporware drinking vinegar prism. Shabby chic tacos artisan, chambray chicharrones cardigan leggings typewriter af pop-up williamsburg meditation PBR&B viral. You probably haven't heard of them DIY jean shorts subway tile fashion axe bushwick kitsch tumeric cloud bread vaporware freegan franzen pork belly chicharrones banh mi.",
+    "Man braid celiac synth freegan readymade, pitchfork fam salvia waistcoat lomo bitters gentrify four loko. Pitchfork semiotics post-ironic vegan. Tofu meditation microdosing hashtag semiotics venmo. Flexitarian vape tilde taiyaki. Prism poutine farm-to-table, messenger bag vegan taxidermy tattooed sartorial squid jean shorts fixie selvage trust fund vape.",
+  ];
+  const inputRef = useRef(0);
+  const [renderPara, setRenderPara] = useState([]);
+
+  const takeInputValue = () => {
+    if (!(inputRef.current.value > 8 || inputRef.current.value < 1))
+      setRenderPara(Paragraphs.slice(0, inputRef.current.value));
+    else {
+      alert("Input valid between 1-8");
+      inputRef.current.value < 1
+        ? setRenderPara(Paragraphs.slice(0, 1))
+        : setRenderPara(Paragraphs);
+    }
+  };
+  return (
+    <div className="flex flex-col gap-5 text-center mt-24 ">
+      <h1 className="text-4xl font-bold text-orange-800 tracking-wider mb-4">
+        TIRED OF BORING LOREM IPSUM?
+      </h1>
+      <div className="flex gap-1 justify-center mb-3">
+        <p className="text-3xl ">Paragraphs:</p>
+        <input
+          type="number"
+          className=" rounded-md mx-2.5 w-20 px-2 py-1"
+          ref={inputRef}
+        />
+        <button
+          className="uppercase text-xl bg-orange-400 py-1.5 px-4 rounded text-white"
+          onClick={takeInputValue}
+        >
+          Generate
+        </button>
+      </div>
+      <ol className="list-decimal">
+        {renderPara.map((para, index) => {
+          return (
+            <li
+              key={index}
+              className="text-xl lg:w-1/2 md:w-3/4 m-auto text-orange-800 mb-4"
+            >
+              {para}
+            </li>
+          );
+        })}
+      </ol>
+    </div>
+  );
+}
